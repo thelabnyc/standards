1e461452c9f9e0f0dc4714294616b4a567a7846c
Craig Weber <crgwbr@gmail.com>
2024-01-30 17:36:08 -0500

    Fix stylelint config typos

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
     customSyntax: "postcss-scss",
     plugins: ["stylelint-scss", "@thelabnyc/standards/rules/stylelint"],
     rules: {
-        "custom-rules/no-composing-sass": true,
+        "thelabnyc/no-composing-sass": true,
     },
 };
 
