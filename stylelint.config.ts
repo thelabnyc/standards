08e292dc475ae1c126bb40293ed9e6093bb3bad4
Craig Weber <crgwbr@gmail.com>
2024-01-30 19:47:26 -0500

    Add a few additional stylelint rules

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -4,8 +4,32 @@ const config: Config = {
     customSyntax: "postcss-scss",
     plugins: ["stylelint-scss", "@thelabnyc/standards/rules/stylelint"],
     rules: {
+        // Built-in rule tweaks
+        "media-feature-range-notation": null,
+        "no-descending-specificity": null,
+        "selector-class-pattern": [
+            "^[a-z][a-zA-Z0-9]+$",
+            {
+                message: "Expected custom property name to be camelCase",
+            },
+        ],
+        "value-keyword-case": null,
+        // Custom Rules
         "thelabnyc/no-composing-sass": true,
     },
+    overrides: [
+        {
+            files: ["**/*.scss"],
+            extends: [
+                "stylelint-config-standard-scss",
+                "stylelint-config-css-modules",
+            ],
+            rules: {
+                "scss/dollar-variable-empty-line-before": null,
+                "scss/no-global-function-names": null,
+            },
+        },
+    ],
 };
 
 export = config;
